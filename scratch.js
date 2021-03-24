44f7717f7c8b3105c4e874f381e34040f3d57f11
John Riewerts <john@acoustics-mbp.attlocal.net>
2021-03-23 21:17:12 -0500

    have fun :)

--- scratch.js
+++ scratch.js
@@ -8,11 +8,17 @@ let entry2 = new BPEntry('Tuesday', 1000, 2, 0, 0);
 let entry3 = new BPEntry('Wednesday', 1000, 2, 0, 0);
 let entry4 = new BPEntry('Thursday', 1000, 2, 0, 0);
 
-db.addEntry(entry);
-db.addEntry(entry2);
-db.addEntry(entry3);
-db.addEntry(entry4);
+db.addEntry(entry);     
+db.addEntry(entry2);    
+db.addEntry(entry3);    
+db.addEntry(entry4);    
 
 let entries = db.getEntries();
+
+
+console.info(entries[0].threeDayAverage); //entry
+console.info(entries[1].threeDayAverage); //e, e2
+console.info(entries[2].threeDayAverage); //e, e2, e3
+console.info(entries[3].threeDayAverage); //e2, e3, e4
 
 console.log(entries);
