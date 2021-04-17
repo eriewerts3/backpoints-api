63bfd5250a7d50f300db3fd082f5c4c7de04628a
eriewerts3 <55771100+eriewerts3@users.noreply.github.com>
2021-04-17 15:54:20 -0700

    getAverage integration with entries bug

--- models/back-points.js
+++ models/back-points.js
@@ -17,47 +17,28 @@ const BPEntry = require('./back-points-entry');
      */
     getAverage(arr) {
 
-        // if we dont use have the array passed in
+        // if we dont use have the array passed in use entries
         if(!arr){
             arr = this.entries;
         }
-
-        let sum = 0;
-
-        for (let i = 0; i < arr.length; i++) {
-            sum = sum + arr[i].da;
+        for (const item of arr) {
+            // if object has property of fouravg continue if now calculate based on last 4 days
+            if (item.hasOwnProperty(fouravg)) {
+                continue;
+            } else {
+                fourdays = []
+                for (let i = 4; i > 0; --i) {
+                    fourdays.push(item[i]);
+                }
+                let sum = 0
+                for (let i = 0; i < fourdays.length; i++) {
+                    sum = sum + arr[i].da;
+                    average4 = sum / fourdays.length;
+                }
+                item[i].fouravg = average4;
+            }
         }
-
-        return sum / arr.length;
     }
-
-    
-    // Funciton that finds the average of an array of any size by using Array.reduce() to add all the numbers in the array then deviding it by the array length
-    // get4DayAverage(arr) {
-        
-    //     for(let i = 0, l = arr.legth; i < l; i++){
-    //         let check_3 = arr[i].has0wnProperty('fourDayAverage');
-            
-    //         if(check_3 === true){
-    //             console.log(arr[i]('fourDayAverage'));
-    //         } else {
-    //            // Funciton that finds the average of an array of any size by using Array.reduce() to add all the numbers in the array then deviding it by the array length
-    //             function getAvg(array) {
-    //                 //TODO ? cycle through last 2 entries to calculate average from an array with all three entry parameters.
-    //                 let sum = array.reduce((a,c) => a + c, 0);
-    //                 return sum / array.length;
-    //             }
-    //             a= [];
-    //             for (let index = 3; index > 0; index--) {
-    //                 act_entry = arr[index]('da');
-    //                 a.push(act_entry);
-    //             }
-    //             let result = getAvg(a);
-    //             console.log('this is the result of for/else loop' + result);
-    //             arr[i]('fourDayAverage') = result;
-    //         }
-    //     }
-    // }
 
     /**
      * adds a new bpentry to the database
@@ -72,6 +53,7 @@ const BPEntry = require('./back-points-entry');
      * @returns 
      */
     getEntries() {
+        this.getAverage(entries);
         return this.entries;
     }
  }
--- scratch.js
+++ scratch.js
@@ -7,28 +7,16 @@ let entry = new BPEntry('Monday', 1000, 2, 0, 0);
 let entry2 = new BPEntry('Tuesday', 1000, 2, 0, 0);
 let entry3 = new BPEntry('Wednesday', 1000, 2, 0, 0);
 let entry4 = new BPEntry('Thursday', 1000, 2, 0, 0);
+let entry5 = new BPEntry('Friday', 2800, 2, 0, 0);
 
 db.addEntry(entry);     
 db.addEntry(entry2);    
 db.addEntry(entry3);    
 db.addEntry(entry4);
+db.addEntry(entry5);
 
 
-let entries = db.getEntries();
-
-function fourDayAverage() {
-    for (const item of entries) {
-        if (item.hasOwnProperty(fourDayAverage)) {
-            continue;
-        } else {
-            fourdays = []
-            for (let i = 4; i > 0; --i) {
-                fourdays.push(item[i]);
-            }
-            getAverage(fourdays);
-        }
-    }
-}
+let entries = db.getEntries();//TODO cant figure out why entries isnt being created
 
 // Console loging entries to practice for of loop for to practice for homework
 function forofEntries() {
@@ -38,11 +26,11 @@ function forofEntries() {
 }
 
 
-console.info(entries[0].fourDayAverage); //entry
-console.info(entries[1].fourDayAverage); //e, e2
-console.info(entries[2].fourDayAverage); //e, e2, e3
-console.info(entries[3].fourDayAverage); //e, e2, e3, e4
-console.info(entries[4].fourDayAverage); //e2, e3, e4, e5
+console.info(entries[0].fouravg); //entry
+console.info(entries[1].fouravg); //e, e2
+console.info(entries[2].fouravg); //e, e2, e3
+console.info(entries[3].fouravg); //e, e2, e3, e4
+console.info(entries[4].fouravg); //e2, e3, e4, e5
 
 
 // console.log(entries);
