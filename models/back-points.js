d7e7d322662d2eaf03f2fecd83df4fe7ab022e77
eriewerts3 <55771100+eriewerts3@users.noreply.github.com>
2021-11-08 17:44:36 -0600

    Mongo model integration (needs testing)

--- models/back-points.js
+++ models/back-points.js
@@ -13,27 +13,36 @@ class BpContents {
     // client;
 
     constructor() {
-        //connection string
-        const uri = "mongodb+srv://dbUser:47SexcstaFkX72Qi@cluster0.wike7.mongodb.net/admin?replicaSet=atlas-f7ubs3-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1"/**TODO need to find login info for cluster see notes */
         
-        //build new mongo connection object
-        this.client = new MongoClient(uri);
-        this.isConnected = false;
-
     }
-
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-      sum = sum + arr[i].da;
-    }
-    return sum / arr.length;
-  }
 
   /**
    * adds a new bpentry to the database
    * @param {BPEntry} newEntry the new entry you're passing to add
    */
+  getAverage(arr) {
+    if(!arr) {
+      arr = this.entries;
+    }
+    
+    let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i].da;
+      }
+      return sum / arr.length;
+  }
+
+
   addEntry(newEntry) {
+    //ensure that we are connected to mongo
+    if(this.isConnected) {
+      //connection string
+      const uri = "mongodb+srv://dbUser:47SexcstaFkX72Qi@cluster0.wike7.mongodb.net/admin?replicaSet=atlas-f7ubs3-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1"/**TODO need to find login info for cluster see notes */
+        
+      //build new mongo connection object
+      this.client = new MongoClient(uri);
+      this.isConnected = false;
+    }
     this.entries.push(newEntry);
     fs.writeFileSync("./myData.json", JSON.stringify(this.entries, null, 4));
   }
@@ -42,69 +51,53 @@ class BpContents {
    * Get you all the entries stored in the database
    * @returns
    */
-  getEntries() {
-    let contents = fs.readFileSync("./myData.json", "utf-8");
+  async getEntries() {
 
-    if (contents.length == 0) {
-      this.entries = [];
-    } else {
-      this.entries = JSON.parse(contents);
-    }
+      //ensure that we are connected to mongo
+      if(!this.isConnected) {
+          await this.client.connect();
+          this.isConnected = true;
+      }
 
-    /**
-     * Get you all the entries stored in the database
-     * @returns 
-     */
-    async getEntries() {
-        
-        //ensure that we are connected to mongo
-        if(!this.isConnected) {
-            await this.client.connect();
-            this.isConnected = true;
-        }
-
-        //let contents = fs.readFileSync('./myData.json', 'utf-8');
-        let contents = await client.db('backPoints').collection('entries').aggregate([{$match: {}}]).toArray();
-        
-        if (contents.length == 0) {
-            this.entries = [];
-        } else {
-            this.entries = JSON.parse(contents);
-        }
+      //let contents = fs.readFileSync('./myData.json', 'utf-8');
+      let contents = await client.db('backPoints').collection('entries').aggregate([{$match: {}}]).toArray();
+      
+      if (contents.length == 0) {
+          this.entries = [];
+      } else {
+          this.entries = JSON.parse(contents);
+      }
 
 
-        for (let i = 0; i < this.entries.length; i++) {
+      for (let i = 0; i < this.entries.length; i++) {
 
-            switch (i) {
-                case 0:
-                    this.entries[i].fourDayAvg = this.entries[0].da;
-                    break;
-                case 1:
-                    this.entries[i].fourDayAvg = this.getAverage([this.entries[0], this.entries[1]]);
-                    break;
-                case 2:
-                    this.entries[i].fourDayAvg = this.getAverage([this.entries[0], this.entries[1], this.entries[2]]);
-                    break;
-                default:
-                    let arr = [];
+          switch (i) {
+              case 0:
+                  this.entries[i].fourDayAvg = this.entries[0].da;
+                  break;
+              case 1:
+                  this.entries[i].fourDayAvg = this.getAverage([this.entries[0], this.entries[1]]);
+                  break;
+              case 2:
+                  this.entries[i].fourDayAvg = this.getAverage([this.entries[0], this.entries[1], this.entries[2]]);
+                  break;
+              default:
+                  let arr = [];
 
-                    //get last 4 items from current index
-                    for (let j = i; j > (i - 4); j--) {
-                        arr.push(this.entries[j]);
-                    }
+                  //get last 4 items from current index
+                  for (let j = i; j > (i - 4); j--) {
+                      arr.push(this.entries[j]);
+                  }
 
-                    this.entries[i].fourDayAvg = this.getAverage(arr);
+                  this.entries[i].fourDayAvg = this.getAverage(arr);
 
-                    break;
-            }
+                  break;
+          }
 
-            const element = this.entries[i];
-        }
+          const element = this.entries[i];
+      }
 
-        return this.entries;
-    }
-
-    return this.entries;
+      return this.entries;
   }
 }
 
--- scratch.js
+++ scratch.js
@@ -4,26 +4,27 @@ const {MongoClient} = require('mongodb');
 const { response } = require("express");
 
 
-async function main() {
-  
-  //connection string
-  const uri = "mongodb+srv://dbUser:47SexcstaFkX72Qi@cluster0.wike7.mongodb.net/admin?replicaSet=atlas-f7ubs3-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1"/**TODO need to find login info for cluster see notes */
-  
-  //build new mongo connection object
-  const client = new MongoClient(uri);
-  
-  // use that new client to connect
-  await client.connect();
-  
-  //use the client to reference the collection object for entries collection
-  const coll = client.db('backPoints').collection('entries');
 
-  let response = await coll.aggregate([{$match: {}}]).toArray();
-
-  console.info(JSON.stringify(response, null, 4));
+// async function main() {
   
-  client.close();
-}
+//   // //connection string
+//   // const uri = "mongodb+srv://dbUser:47SexcstaFkX72Qi@cluster0.wike7.mongodb.net/admin?replicaSet=atlas-f7ubs3-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1"/**TODO need to find login info for cluster see notes */
+  
+//   // //build new mongo connection object
+//   // const client = new MongoClient(uri);
+  
+//   // // use that new client to connect
+//   // await client.connect();
+  
+//   //use the client to reference the collection object for entries collection
+//   // const coll = client.db('backPoints').collection('entries');
+
+//   // let response = await coll.aggregate([{$match: {}}]).toArray();
+
+//   // console.info(JSON.stringify(response, null, 4));
+  
+//   // client.close();
+// }
 
 
 (async () => {
